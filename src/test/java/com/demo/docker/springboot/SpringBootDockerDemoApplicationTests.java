3198a1286f349b783d26538275cd3a5639b0d2b6
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2025-11-23 00:04:41 +0500

    added basic spring boot project config files

---
+++ src/test/java/com/demo/docker/springboot/SpringBootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.docker.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
